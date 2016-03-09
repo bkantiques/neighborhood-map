a4df677e7d0233a573e04f00c9eceb40ddf62778
JReed <jreed0719@gmail.com>
2016-03-08 23:10:02 -0500

    First commit of non-build files

---
+++ src/js/NeighborhoodDataValidator.js
@@ -0,0 +1,57 @@
+// Module to check if neighborhood data is valid/ has all required info
+var NeighborhoodDataValidator = (function() {
+
+  var nv = {};
+
+  // Check if a variable is a valid latitude or longitude
+  nv.validLatitude = function(coordinate) {
+    return typeof(coordinate) === 'number' && coordinate >= -90 && coordinate <= 90;
+  };
+
+  nv.validLongitude = function(coordinate) {
+    return typeof(coordinate) === 'number' && coordinate >= -180 && coordinate <= 180;
+  };
+
+  // Check if neighborhood object has valid coordinate boundaries
+  nv.validCoordinates = function(neighborhood) {
+
+    var isValid = true;
+
+    // Check if neighborhood has necessary geometry properties
+    if(neighborhood.geometry && neighborhood.geometry.viewport &&
+      neighborhood.geometry.viewport.getSouthWest && neighborhood.geometry.viewport.getNorthEast &&
+      neighborhood.geometry.viewport.getSouthWest().lat && neighborhood.geometry.viewport.getSouthWest().lng &&
+      neighborhood.geometry.viewport.getNorthEast().lat && neighborhood.geometry.viewport.getNorthEast().lng) {
+
+      var viewport = neighborhood.geometry.viewport;
+
+      // Check if coordinates are valid
+      var minLatitude = viewport.getSouthWest().lat();
+      var maxLatitude = viewport.getNorthEast().lat();
+      var minLongitude = viewport.getSouthWest().lng();
+      var maxLongitude = viewport.getNorthEast().lng();
+
+      if(!(this.validLatitude(minLatitude) && this.validLatitude(maxLatitude) &&
+        this.validLongitude(minLongitude) && this.validLongitude(maxLongitude))) {
+        isValid = false;
+      }
+    }
+    else {
+      isValid = false;
+    }
+
+    return isValid
+  }
+
+  // Check that name and address are strings (as expected)
+  nv.validNameAndAddress = function(neighborhood) {
+    return typeof(neighborhood.name) === 'string' && typeof(neighborhood.formatted_address) === 'string';
+  }
+
+  // Check if neighborhood has valid, required properties
+  nv.validNeighborhood = function(neighborhood) {
+    return this.validCoordinates(neighborhood) && this.validNameAndAddress(neighborhood);
+  }
+
+  return nv;
+})();
---
+++ src/js/PlaceDataValidator.js
@@ -0,0 +1,58 @@
+  // Module to check if neighborhood data is valid/ has all required info
+  var PlaceDataValidator = (function(place) {
+    var pv = {};
+
+    // Check that name and url are valid
+    pv.validStringProperties = function(place) {
+      return typeof(place.name) === 'string' &&
+      typeof(place.url) === 'string';
+    };
+
+    pv.validLocation = function(place) {
+      return place.location &&
+        place.location.address && typeof(place.location.address[0]) === 'string';
+    }
+
+    // Check if a variable is a valid latitude or longitude
+    pv.validLatitude = function(coordinate) {
+      return typeof(coordinate) === 'number' && coordinate >= -90 && coordinate <= 90;
+    };
+
+    pv.validLongitude = function(coordinate) {
+      return typeof(coordinate) === 'number' && coordinate >= -180 && coordinate <= 180;
+    };
+
+    // Check if place has expected, valid coordinates
+    pv.validCoordinate = function(place) {
+      if(place.location && place.location.coordinate && place.location.coordinate && place.location.coordinate.latitude && place.location.coordinate.longitude) {
+        var latitude = place.location.coordinate.latitude;
+        var longitude = place.location.coordinate.longitude;
+
+        return this.validLatitude(latitude) && this.validLongitude(longitude);
+      }
+      else {
+        return false;
+      }
+    };
+
+    // Check that rating is valid
+    pv.validRating = function(place) {
+      return typeof(place.rating) === "number" && place.rating >= 0 && place.rating <= 5;
+    };
+
+    // Check that categories array has at least 1 entry with a name and code
+    pv.validCategories = function(place) {
+      return place.categories && place.categories[0] && typeof(place.categories[0][0]) === 'string' && typeof(place.categories[0][1]) === 'string';
+    };
+
+    // Check that place has all required valid properties
+    pv.validPlace = function(place) {
+      return this.validStringProperties(place) &&
+        this.validLocation(place) &&
+        this.validRating(place) &&
+        this.validCategories(place) &&
+        this.validCoordinate(place);
+    };
+
+    return pv;
+  })();
